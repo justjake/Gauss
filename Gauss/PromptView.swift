dc991ef358a98578c434bae2e9992934cc301f10
Jake Teton-Landis <just.1.jake@gmail.com>
2022-12-03 10:30:18 -0800

    init

---
+++ Gauss/PromptView.swift
@@ -0,0 +1,20 @@
+//
+//  PromptView.swift
+//  Gauss
+//
+//  Created by Jake Teton-Landis on 12/3/22.
+//
+
+import SwiftUI
+
+struct PromptView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PromptView_Previews: PreviewProvider {
+    static var previews: some View {
+        PromptView()
+    }
+}
---
+++ Gauss/ResultView.swift
@@ -0,0 +1,20 @@
+//
+//  ResultView.swift
+//  Gauss
+//
+//  Created by Jake Teton-Landis on 12/3/22.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultView()
+    }
+}
